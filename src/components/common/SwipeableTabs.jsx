e7710f60164ed3b805d2e4c18fc1bf26c8292dde
Aryeh Kohn <aryeh18kohn@gmail.com>
2026-02-16 23:58:44 +0000

    File changes

--- src/components/common/SwipeableTabs.jsx
+++ src/components/common/SwipeableTabs.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence, PanInfo } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function SwipeableTabs({ 
   tabs, 
