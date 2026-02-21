4d8774674a307f488762fe849214bfd1097a6166
Aryeh Kohn <aryeh18kohn@gmail.com>
2026-02-21 21:30:25 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -9,7 +9,7 @@ import SwipeableTabs from '@/components/common/SwipeableTabs';
 // Lazy load main pages
 const Feed = lazy(() => import('@/pages/Feed'));
 const CommunityUpdates = lazy(() => import('@/pages/CommunityUpdates'));
-const Communities = lazy(() => import('@/pages/Communities'));
+const Communities = lazy(() => import('@/pages/Communities.jsx'));
 const MitzvahCircle = lazy(() => import('@/pages/MitzvahCircle'));
 const Profile = lazy(() => import('@/pages/Profile'));
 
